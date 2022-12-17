87b8ae59ee3e88f45e0cf279254ac0c4e6806954
JlpoCTak <JlpocTaK@yandex.ru>
2022-12-17 23:41:50 +0300

    1

---
+++ documents.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<body>
+        <style type="text/css"> 
+  button[name="run_script"] { 
+    border: none;
+    border-radius: 70px;
+    padding: 10px 25px;
+    background: #ff9900;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: white;
+  }
+  button[name="run_script"]:hover { 
+    background: #ff6600;
+  } 
+</style>
+<button type='button' name='run_script'>открыть архив</button>
+<script>
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+document.body.querySelector('button[name=run_script]').addEventListener('click', () => window.location.replace('F:/OSPanel/domains/ittech1234/afm/'));
+});
+</script>
+</body>
+</script>
+</html>
